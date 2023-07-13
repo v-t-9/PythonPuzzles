b285feb1d395e233bf56153c71d001ecad9e2b65
v-t-9 <violeta.a.t.9@gmail.com>
2023-07-13 09:50:14 -0300

    ex63

---
+++ ex63.py
@@ -0,0 +1,15 @@
+# Write a Python program to find the sum of the even elements that are at odd indices in a given list.
+# Input:
+l1 = [1, 2, 3, 4, 5, 6, 7]
+# Output:
+# 12
+# Input:
+l2 = [1, 2, 8, 3, 9, 4]
+# Output:
+# 6
+
+def even_elements_odd_indices(l):
+    res = [sum(l[i] for i in range(len(l)) if i % 2 !=0 and l[i] % 2 == 0)]
+    return res[0]
+print(even_elements_odd_indices(l1))
+print(even_elements_odd_indices(l2))
