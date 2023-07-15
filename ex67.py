5c67c31ea020e19ea28fc8bacc39d343ba96347a
v-t-9 <violeta.a.t.9@gmail.com>
2023-07-15 11:10:49 -0300

    ex67

---
+++ ex67.py
@@ -0,0 +1,20 @@
+# Write a Python program to find a string which, when each character is shifted (ASCII incremented) by shift, gives the result.
+# Input:
+s1 = "Ascii character table"
+shift1 = -1
+# Output:
+# @rbhhbg`q`bsdqs`akd
+# Input:
+s2 =  "Ascii character table"
+shift2 = 1
+# Output:
+# Btdjj!dibsbdufs!ubcmf
+
+def ASCII_incremented(s, shift):
+    num = [ord(i) + shift for i in s]
+    res = [chr(i) for i in num]
+    r = "".join(res)
+    return r
+
+print(ASCII_incremented(s1,shift1))
+print(ASCII_incremented(s2,shift2))
