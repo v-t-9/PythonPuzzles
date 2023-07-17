d252c454ec4010e16d4bc028461aa68129410ea3
v-t-9 <violeta.a.t.9@gmail.com>
2023-07-17 17:27:30 -0300

    ex68/ex69

---
+++ ex68.py
@@ -0,0 +1,32 @@
+# Write a Python program to find all 5's in integers less than n that are divisible by 9 or 15.
+# Input:
+n1 = 50
+# Output:
+# [[15, 1], [45, 1]]
+# Input:
+n2 = 65
+# Output:
+# [[15, 1], [45, 1], [54, 0]]
+# Input:
+n3 = 75
+# Output:
+# [[15, 1], [45, 1], [54, 0]]
+# Input:
+n4 = 85
+# Output:
+# [[15, 1], [45, 1], [54, 0], [75, 1]]
+# Input:
+n5 = 150
+# Output:
+# [[15, 1], [45, 1], [54, 0], [75, 1], [105, 2], [135, 2]]
+
+def divisible_by_9_or_15(n):
+    l = [ i for i in range(0,n) if "5" in str(i)]
+    div = [str(l[i]) for i in range(len(l)) if l[i] % 15 == 0 or l[i] % 9 == 0]
+    res = [[int(i),j] for i in div for j in range(len(i)) if i[j] == "5"]
+    return res
+print(divisible_by_9_or_15(n1))
+print(divisible_by_9_or_15(n2))
+print(divisible_by_9_or_15(n3))
+print(divisible_by_9_or_15(n4))
+print(divisible_by_9_or_15(n5))
---
+++ ex69.py
@@ -0,0 +1,17 @@
+# Write a Python program to create a new string by taking a string, and word by word rearranging its characters in ASCII order.
+s1 = "Ascii character table"
+# Output:
+# Aciis aaccehrrt abelt
+s2 = "maltos won"
+# Output:
+# almost now
+
+def rearranging_characters(s):
+    l = s.split(" ")
+    res = ""
+    l1 = [sorted(i) for i in l]
+    r = ["".join(i) for i in l1]
+    res = " ".join(r) 
+    return res
+print(rearranging_characters(s1))
+print(rearranging_characters(s2))
