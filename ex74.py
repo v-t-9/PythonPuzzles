799d09aa932d1eb2dc57fdecd8e1115a735d7452
v-t-9 <violeta.a.t.9@gmail.com>
2023-07-19 19:09:19 -0300

    ex74

---
+++ ex74.py
@@ -0,0 +1,19 @@
+# Write a Python program to find a string consisting of space-separated characters with given counts.
+d1 ={'f': 1, 'o': 2}
+# Output:
+# f o o
+d2 = {'a': 1, 'b': 1, 'c': 1}
+# Output:
+# a b c
+
+def string_space_separated_characters(d):
+    key = d.keys()
+    r = " "
+    res = " "
+    for i in key:
+        r = r + d[i]*i 
+    for i in r:
+        res = res + i + " "
+    return res
+print(string_space_separated_characters(d1))
+print(string_space_separated_characters(d2))
