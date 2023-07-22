889a971301bde81df4c4212fffae6201242888de
v-t-9 <violeta.a.t.9@gmail.com>
2023-07-22 09:34:23 -0300

    ex81

---
+++ ex81.py
@@ -0,0 +1,21 @@
+# Write a Python program to calculate the average of the numbers a through b (b not included) rounded to the nearest integer, in binary (or -1 if there are no such numbers).
+# Input:
+n1 = 4
+n2 = 7
+# Output:
+# 0b101
+# Input:
+n3 = 11 
+n4 = 19
+# Output:
+# 0b1110
+
+def avg_bin(a, b):
+    l = [ i for i in range(a,b)]
+    avg = round(sum(l)/len(l))
+    b_avg = bin(avg)
+    return b_avg
+
+if __name__ == "__main__":
+    print(avg_bin(n1,n2))
+    print(avg_bin(n3,n4))
