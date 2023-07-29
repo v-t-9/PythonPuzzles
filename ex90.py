828b03f0d843b4398677dc4e20d6f0920b5e2908
v-t-9 <violeta.a.t.9@gmail.com>
2023-07-29 14:00:53 -0300

    ex90/ex91

---
+++ ex90.py
@@ -0,0 +1,13 @@
+# For each triple of eaten, need, and stock, write a Python program to get a pair of total appetite and remaining.
+# Input:
+# [[2, 5, 6], [3, 9, 22]]
+# Output:
+# [[7, 1], [12, 13]]
+# Input:
+# [[2, 3, 18], [4, 9, 2], [2, 5, 7], [3, 8, 12], [4, 9, 106]]
+# Output:
+# [[5, 15], [6, 0], [7, 2], [11, 4], [13, 97]]
+# Input:
+# [[1, 2, 3], [4, 5, 6]]
+# Output:
+# [[3, 1], [9, 1]]
