11b3a74a1ae009d7741cdd1ddb0953be2f73c059
v-t-9 <violeta.a.t.9@gmail.com>
2023-07-31 13:51:52 -0300

    ex93

---
+++ ex93.py
@@ -0,0 +1,38 @@
+# Write a Python program to find the closest palindrome to a given string.
+# Input:
+s1 = "cat"
+# Output:
+# cac
+# Input:
+s2 = "madan"
+# Output:
+# madam
+# Input:
+s3 = "radivider"
+# Output:
+# radividar
+# Input:
+s4 = "abc"
+# Output:
+# aba
+# Input:
+s5 = "racecbr"
+# Output:
+# racecar
+
+def closest_palindrome(s):
+    res = ""
+    mid = int(len(s)/2)
+    mb = s[:mid]
+    res = res + mb + s[mid] + mb[::-1]
+
+    return res
+
+
+
+if __name__ == "__main__":
+    print(closest_palindrome(s1))
+    print(closest_palindrome(s2))
+    print(closest_palindrome(s3))
+    print(closest_palindrome(s4))
+    print(closest_palindrome(s5))
