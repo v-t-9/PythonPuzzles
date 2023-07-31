fb4e85c8fec82c11e424e7a094a246d25fdfdc7d
v-t-9 <violeta.a.t.9@gmail.com>
2023-07-31 16:07:37 -0300

    ex94/ex95

---
+++ ex94.py
@@ -0,0 +1,9 @@
+# Given a string consisting of whitespace and groups of matched parentheses, write a Python program to split it into groups of perfectly matched parentheses without any whitespace.
+# Input:
+# ( ()) ((()()())) (()) ()
+# Output:
+# ['(())', '((()()()))', '(())', '()']
+# Input:
+# () (( ( )() ( )) ) ( ())
+# Output:
+# ['()', '((()()()))', '(())']
---
+++ ex95.py
@@ -0,0 +1,46 @@
+# Write a Python program to find a palindrome of a given length containing a given string.
+# Input: 
+s1 = "madam"
+n1 = 7
+# Output:
+# madaadam
+# Input: madam , 
+n2 = 6
+# Output:
+# maddam
+# Input: madam , 
+n3 = 5
+# Output:
+# maaaam
+# Input: madam , 
+n4 = 3
+# Output:
+# maam
+# Input: madam ,
+n5 = 2
+# Output:
+# mm
+# Input: madam , 
+n6 = 1
+# Output:
+# aa
+
+def len_palindrome(s,n):
+ res = ""
+ leng = n // 2
+ for i in range(len(s)):
+    if len(res)<leng:
+        res = res + s[i]
+ if n%2 != 0:
+    res = res + "a"
+
+ 
+ return res + res[::-1]
+
+if __name__ == "__main__":
+    print(len_palindrome(s1,n1))
+    print(len_palindrome(s1,n2))
+    print(len_palindrome(s1,n3))
+    print(len_palindrome(s1,n4))
+    print(len_palindrome(s1,n5))
+    print(len_palindrome(s1,n6))
